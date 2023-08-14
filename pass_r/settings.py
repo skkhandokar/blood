0d10a0b65cd38a0e24242d29749fbd6d222251b9
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2023-08-14 15:40:41 +0600

    Update settings.py

--- pass_r/settings.py
+++ pass_r/settings.py
@@ -157,9 +157,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'khandokarsamad4@gmail.com'
-EMAIL_HOST_PASSWORD = 'jugklypgckhwdcgq'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'OTP Team <khandokarsamad4@gmail.com>'
+DEFAULT_FROM_EMAIL = 
 
 # py manage.py runserver
